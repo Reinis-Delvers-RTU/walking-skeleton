cc3caa58e70d07de0bca5be14816947e574138a8
Reinis-Delvers-RTU <reinis.delvers@edu.rtu.lv>
2025-11-27 21:07:40 +0200

    .

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import { Hono } from '@hono/hono';
 import { cors } from '@hono/hono/cors';
 import { logger } from '@hono/hono/logger';
 import postgres from 'postgres';
+import { cache } from '@hono/hono/cache';
 
 const sql = postgres();
 const app = new Hono();
@@ -14,5 +15,30 @@ app.get("/todos", async (c) => {
     const todos = await sql`SELECT * FROM todos`;
     return c.json(todos);
 })
+
+app.get("/cache-demo", cache({
+    cacheName: "demo-cache",
+    wait: true,
+}))
+
+app.get("/cache-demo", async (c) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return c.json({message: "Cache demo"});
+});
+
+app.get("/hello/*", cache({
+    cacheName: "hello-cache",
+    wait: true,
+}))
+
+app.get("/hello/:name", async (c) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return c.json({message: `Hello ${c.req.param("name")}`});
+});
+
+app.post("/hello", async (c) => {
+    await caches.delete("hello-cache");
+    return c.json({message: "Hello Cache cleared"});
+});
 
 export default app;
