6278b8d6350664d476162ede666ae1010fb9cdcb
Reinis-Delvers-RTU <reinis.delvers@edu.rtu.lv>
2025-12-02 20:45:15 +0200

    .

--- server/app.js
+++ server/app.js
@@ -2,10 +2,27 @@ import { Hono } from '@hono/hono';
 import { cors } from '@hono/hono/cors';
 import { logger } from '@hono/hono/logger';
 import postgres from 'postgres';
-import { cache } from '@hono/hono/cache';
+import { Redis } from 'ioredis';
+import { redisCacheMiddleware } from './cache-middleware.js';
 
 const sql = postgres();
 const app = new Hono();
+const redis = new Redis(6379, 'redis');
+
+app.get('/redis-test', async (c) => {
+    let count = await redis.get('test');
+    if (!count) {
+        count = '0';
+    }
+    else {
+        count = Number(count);
+    }
+
+    count++;
+
+    await redis.set('test', count);
+    return c.json({ count });
+});
 
 app.use('/*', cors());
 app.use('/*', logger());
@@ -14,31 +31,14 @@ app.get('/', (c) => c.json({message: 'Hello world!'}));
 app.get("/todos", async (c) => {
     const todos = await sql`SELECT * FROM todos`;
     return c.json(todos);
-})
-
-app.get("/cache-demo", cache({
-    cacheName: "demo-cache",
-    wait: true,
-}))
-
-app.get("/cache-demo", async (c) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return c.json({message: "Cache demo"});
 });
 
-app.get("/hello/*", cache({
-    cacheName: "hello-cache",
-    wait: true,
-}))
+app.get("/hello/*", redisCacheMiddleware,);
 
 app.get("/hello/:name", async (c) => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    return c.json({message: `Hello ${c.req.param("name")}`});
-});
-
-app.post("/hello", async (c) => {
-    await caches.delete("hello-cache");
-    return c.json({message: "Hello Cache cleared"});
+    return c.json({ message: `Hello ${c.req.param("name")}!` });
 });
 
 export default app;
+export { redis };
